7b3d37486a6be944e2f24050e7b3a5214182aa05
Daniel Leal <danielleal94@gmail.com>
2019-04-29 18:33:22 -0300

    init repo

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'leal',
+  password: '123',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAl: true
+  }
+}
